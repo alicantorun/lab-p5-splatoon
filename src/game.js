83b64c3ffe125e54077f6fbb9fc4f95c1b59b693
Alican Torun <torun.alican@gmail.com>
2019-06-14 15:57:18 +0200

    done

--- src/game.js
+++ src/game.js
@@ -3,5 +3,75 @@ class Game {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+
+    //background(220);
+    //For (var BEGIN; END; INTERVAL){
+    //DO SOMETHING }
+    for (let x = 0; x < WIDTH; x += WIDTH / 10) {
+      line(x, 0, x, WIDTH);
+
+      for (let y = 0; y < WIDTH; y += WIDTH / 10) {
+        line(0, y, WIDTH, y);
+      }
+    }
+  }
+}
+
+class Player {
+  constructor(col, row, ori, arrOfCoordinates, color) {
+    this.col = col;
+    this.row = row;
+    this.ori = ori;
+    this.arrOfCoordinates = arrOfCoordinates;
+    this.color = color;
+  }
+
+  moveUp() {
+    this.col -= 100;
+    this.ori = "up";
+    this.arrOfCoordinates.push(this.col, this.row);
+    clear();
+  }
+
+  moveDown() {
+    this.col += 100;
+    this.ori = "down";
+    this.arrOfCoordinates.push(this.col, this.row);
+    clear();
+  }
+
+  moveLeft() {
+    this.row -= 100;
+    this.ori = "left";
+    this.arrOfCoordinates.push(this.col, this.row);
+    clear();
+  }
+
+  moveRight() {
+    this.row += 100;
+    this.ori = "right";
+    this.arrOfCoordinates.push(this.col, this.row);
+    clear();
+  }
+
+  draw() {
+    for (let i = 0; i < this.arrOfCoordinates.length; i += 2) {
+      rect(this.arrOfCoordinates[i + 1], this.arrOfCoordinates[i], 100, 100);
+      fill(this.color);
+    }
+
+    if (this.ori === "down") {
+      image(img_down, this.row, this.col);
+      img_down.resize(100, 100);
+    } else if (this.ori === "up") {
+      image(img_up, this.row, this.col);
+      img_up.resize(100, 100);
+    } else if (this.ori === "left") {
+      image(img_left, this.row, this.col);
+      img_left.resize(100, 100);
+    } else if (this.ori === "right") {
+      image(img_right, this.row, this.col);
+      img_right.resize(100, 100);
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,43 @@
 const game = new Game();
+const player = new Player(0, 0, "down", [], "red");
+const player_enemy = new Player(900, 900, "down", [], "blue");
+
+let img_down = "../assets/character-down.png";
+let img_up = "../assets/character-up.png";
+let img_left = "../assets/character-left.png";
+let img_right = "../assets/character-right.png";
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  img_down = loadImage(img_down); // Load the image
+  img_up = loadImage(img_up); // Load the image
+  img_left = loadImage(img_left); // Load the image
+  img_right = loadImage(img_right); // Load the image
 }
 
 function draw() {
   game.drawGrid();
+  player.draw();
+  player_enemy.draw();
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    player.moveLeft();
+  } else if (keyCode === RIGHT_ARROW) {
+    player.moveRight();
+  } else if (keyCode === UP_ARROW) {
+    player.moveUp();
+  } else if (keyCode === DOWN_ARROW) {
+    player.moveDown();
+  } else if (keyCode === 87) {
+    player_enemy.moveUp();
+  } else if (keyCode === 83) {
+    player_enemy.moveDown();
+  } else if (keyCode === 65) {
+    player_enemy.moveLeft();
+  } else if (keyCode === 68) {
+    player_enemy.moveRight();
+  }
 }
